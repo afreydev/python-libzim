fb24a0f749a924778d804ce68b8e573cd459055d
Nick Sweeting <git@sweeting.me>
2020-04-21 16:26:17 -0400

    add Github actions for lint, test, and release

--- run_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf tests/kiwix-test-*
-
-python3 tests/test_libzim.py
-rm -rf tests/kiwix-test-*
--- tests/test_libzim.py
+++ tests/test_libzim.py
@@ -1,36 +1,23 @@
-import unittest
-import os,sys,inspect
+#!/usr/bin/env python3
 
-# Import local libzim module from parent
-current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, parent_dir)
+import os
+import sys
+import tempfile
+import pathlib
+import uuid
+import shutil
+import unittest
+from ctypes.util import find_library
+
+BASE_DIR = pathlib.Path(__file__).parent
+sys.path.insert(0, str(BASE_DIR / 'cython_build'))
+sys.path.insert(0, str(BASE_DIR))
+
+if not find_library('zim'):
+    raise ImportError('Missing libzim.so or libzim.a dylib in system libs')
 
 from libzim import ZimArticle, ZimBlob, ZimCreator
 
-# test files https://wiki.kiwix.org/wiki/Content_in_all_languages
-
-
-# https://wiki.openzim.org/wiki/Metadata
-
-TEST_METADATA = { 
-    # Mandatory
-    "Name" : "wikipedia_fr_football",
-    "Title": "English Wikipedia",
-    "Creator": "English speaking Wikipedia contributors",
-    "Publisher": "Wikipedia user Foobar",
-    "Date": "2009-11-21",
-    "Description": "All articles (without images) from the english Wikipedia",
-    "Language": "eng",
-    # Optional
-    "Longdescription": "This ZIM file contains all articles (without images) from the english Wikipedia by 2009-11-10. The topics are ...",
-    "Licence": "CC-BY",
-    "Tags": "wikipedia;_category:wikipedia;_pictures:no;_videos:no;_details:yes;_ftindex:yes",
-    "Flavour": "nopic",
-    "Source": "https://en.wikipedia.org/",
-    "Counter": "image/jpeg=5;image/gif=3;image/png=2",
-    "Scraper": "sotoki 1.2.3"
-}
 
 class ZimTestArticle(ZimArticle):
     content = '''<!DOCTYPE html> 
@@ -39,9 +26,6 @@ class ZimTestArticle(ZimArticle):
                 <title>Monadical</title>
                 </head>
                 <h1> ñññ Hello, it works ñññ </h1></html>'''
-
-    def __init__(self):
-        ZimArticle.__init__(self)
 
     def is_redirect(self):
         return False
@@ -74,45 +58,83 @@ class ZimTestArticle(ZimArticle):
 
 class TestZimCreator(unittest.TestCase):
     def setUp(self):
-        self.test_zim_file_path = "/opt/python-libzim/tests/kiwix-test"
-         
-        # Test article
-        self.test_article =  ZimTestArticle()
+        self.test_dir = tempfile.mkdtemp()
+        self.test_article = ZimTestArticle()
 
     def tearDown(self):
-        pass
-
-    def _assert_article_properties(self, written_article, article):
-        pass
-    
-    def _add_article_to_test_zim_file_read_it_back(self, article, delete_zim_file=True):
-        pass
+        shutil.rmtree(self.test_dir)
 
     def test_write_article(self):
-        import uuid
-        rnd_str = str(uuid.uuid1()) 
-        zim_creator = ZimCreator(self.test_zim_file_path + '-' + rnd_str + '.zim',main_page = "welcome",index_language= "eng", min_chunk_size= 2048)
+        zim_creator = ZimCreator(
+            os.path.join(self.test_dir, 'test_zim_creator.zim'),
+            main_page="welcome",
+            index_language="eng",
+            min_chunk_size=2048,
+        )
         zim_creator.add_article(self.test_article)
         # Set mandatory metadata
-        zim_creator.update_metadata(creator='python-libzim',description='Created in python',name='Hola',publisher='Monadical',title='Test Zim')
+        zim_creator.update_metadata(
+            creator='python-libzim',
+            description='Created in python',
+            name='Hola',
+            publisher='Monadical',
+            title='Test Zim',
+        )
         zim_creator.finalize()
 
     def test_article_metadata(self):
-        import uuid
-        rnd_str = str(uuid.uuid1()) 
-        zim_creator = ZimCreator(self.test_zim_file_path + '-' + rnd_str + '.zim',main_page = "welcome",index_language= "eng", min_chunk_size= 2048)
+        zim_creator = ZimCreator(
+            os.path.join(self.test_dir, f'test-metadata-{uuid.uuid1()}.zim'),
+            main_page="welcome",
+            index_language="eng",
+            min_chunk_size= 2048,
+        )
         zim_creator.update_metadata(**TEST_METADATA)
         self.assertEqual(zim_creator._get_metadata(), TEST_METADATA)
 
     def test_check_mandatory_metadata(self):
-        import uuid
-        rnd_str = str(uuid.uuid1()) 
-        zim_creator = ZimCreator(self.test_zim_file_path + '-' + rnd_str + '.zim',main_page = "welcome",index_language= "eng", min_chunk_size= 2048)
+        zim_creator = ZimCreator(
+            os.path.join(self.test_dir, f'test-metadata-{uuid.uuid1()}.zim'),
+            main_page="welcome",
+            index_language="eng",
+            min_chunk_size= 2048,
+        )
         self.assertFalse(zim_creator.mandatory_metadata_ok())
-        zim_creator.update_metadata(creator='python-libzim',description='Created in python',name='Hola',publisher='Monadical',title='Test Zim')
+        zim_creator.update_metadata(
+            creator='python-libzim',
+            description='Created in python',
+            name='Hola',
+            publisher='Monadical',
+            title='Test Zim',
+        )
         self.assertTrue(zim_creator.mandatory_metadata_ok())
 
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+# TODO: test larger zim datasets
+# https://wiki.kiwix.org/wiki/Content_in_all_languages
+# https://wiki.openzim.org/wiki/Metadata
+
+TEST_METADATA = { 
+    # Mandatory
+    "Name" : "wikipedia_fr_football",
+    "Title": "English Wikipedia",
+    "Creator": "English speaking Wikipedia contributors",
+    "Publisher": "Wikipedia user Foobar",
+    "Date": "2009-11-21",
+    "Description": "All articles (without images) from the english Wikipedia",
+    "Language": "eng",
+    # Optional
+    "LongDescription": "This ZIM file contains all articles (without images) from the english Wikipedia by 2009-11-10. The topics are ...",
+    "Licence": "CC-BY",
+    "Tags": "wikipedia;_category:wikipedia;_pictures:no;_videos:no;_details:yes;_ftindex:yes",
+    "Flavour": "nopic",
+    "Source": "https://en.wikipedia.org/",
+    "Counter": "image/jpeg=5;image/gif=3;image/png=2",
+    "Scraper": "mwoffliner 1.2.3"
+}
